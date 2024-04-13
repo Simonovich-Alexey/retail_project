b1b4c3aca208cbdc95986b0c207d4297085a813c
Alexey_Sim <simonovich_alexey@bk.ru>
2024-04-13 23:26:10 +0500

    Add views RgisterUser

--- retail_project/users/urls.py
+++ retail_project/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from users.views import RegisterUser
 
 
+app_name = 'users'
 
+urlpatterns = [
+    path('register/', RegisterUser.as_view(), name='register'),
+]
--- retail_project/users/views.py
+++ retail_project/users/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from users.models import User
+
+
+class RegisterUser(APIView):
+    def post(self, request, *args, **kwargs):
+        create_user = User.objects.create_user()
+        return Response({'message': 'OK'})
